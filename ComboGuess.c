b5b296d14cc999e87f710a8d30e6431ecb1b2c7a
benmccormack <71874270+benmccormack@users.noreply.github.com>
2021-03-02 21:24:38 +0000

    Update ComboGuess.c

--- ComboGuess.c
+++ ComboGuess.c
@@ -5,7 +5,7 @@ Exam Replacement Assignment.
 A program which takes the users combination code, encrypts it and compares it to the already encrypted access code before
 decrypting the users code again. The amount of correct and incorrect entires made by the user is recorded.
 -------
-By Ben McCormack C19459424 
+By Ben McCormack
 22.4.2020 
 */
 
